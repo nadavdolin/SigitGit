d4a776d30a764e8087ccbf883953e69e09528b97
nadavdolin <nadavdolin1@gmail.com>
2019-02-25 10:51:32 +0200

    Exercise5 added

---
+++ Exercise5.py
@@ -0,0 +1,37 @@
+import math
+
+def roundup(x):
+    return int(math.ceil(x / 10.0)) * 10
+
+def check_id():
+    ID=input('Please enter your ID\n')
+    right=int(ID[-1:])
+    ID=int(ID)/10
+    ID=round(ID-0.5)
+    temp_ID=list(str(ID))
+    temp=0
+    sum1=0
+    num=1
+    for i in temp_ID:
+        temp=int(i)*num
+        if temp>=10:
+            minisum=list(str(temp))
+            for j in minisum:
+                sum1+=int(j)
+        elif temp<10:
+         sum1+=temp
+        if num==1:
+            num+=1
+        else:
+            num-=1
+    roundnum = roundup(sum1)
+    if roundnum-sum1==right:
+        return 1
+    else:
+        return 0
+
+answer=check_id()
+if answer==1:
+    print('the ID is ok')
+else:
+    print('the ID is not ok')
