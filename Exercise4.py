64f7ec0603ceacf88207643b1ee60705d440b333
nadavdolin <nadavdolin1@gmail.com>
2019-02-25 10:51:06 +0200

    Exercise4 added

---
+++ Exercise4.py
@@ -0,0 +1,17 @@
+def convert(string):
+    count=0
+    new_word=''
+    index=0
+    for index in range(len(string)-1):
+        if string[index]==string[index+1]:
+            count+=1
+        else:
+            new_word=new_word + string[index] + str(count)
+            count=1
+    new_word = new_word + string[index] + str(count)
+    return str(new_word)
+
+
+s='aaaaabbbccddtt'
+new=convert(s)
+print(str(new))
