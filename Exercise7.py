f17552c666d01067088d190dbf821467fdee3b90
nadavdolin <nadavdolin1@gmail.com>
2019-03-06 20:37:10 +0200

    Update exercise7

--- Exercise7.py
+++ Exercise7.py
@@ -1,11 +1,14 @@
-
+#Input the new Key and value in the cache
 def InputToCache(key,value):
      cache[key]=int(value)
 
+	
 def DecoratorCache(func,data):
     key=func+data
+	#check if the we have answer in the cache
     if key in cache:
         return cache[key]
+	#make the fund and save the new answer
     else:
         data_split=data.split(',')
         value=adding(data_split)
