a5007c96a090aad7d54ef55e094df5b8774483f3
nadavdolin <nadavdolin1@gmail.com>
2019-03-06 20:36:38 +0200

    Update exercise5

--- Exercise5.py
+++ Exercise5.py
@@ -1,17 +1,20 @@
 import math
-
+#Round up a number
 def roundup(x):
     return int(math.ceil(x / 10.0)) * 10
 
+#check if the ID is ok
 def check_id():
     ID=input('Please enter your ID\n')
     right=int(ID[-1:])
     ID=int(ID)/10
     ID=round(ID-0.5)
+    #Split the ID
     temp_ID=list(str(ID))
     temp=0
     sum1=0
     num=1
+
     for i in temp_ID:
         temp=int(i)*num
         if temp>=10:
@@ -25,6 +28,7 @@ def check_id():
         else:
             num-=1
     roundnum = roundup(sum1)
+    #check the number
     if roundnum-sum1==right:
         return 1
     else:
