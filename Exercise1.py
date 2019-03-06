7ece0a1320d0526c4d953c223b9862fccbf75cb5
nadavdolin <nadavdolin1@gmail.com>
2019-03-06 20:33:19 +0200

    Update exercise1

--- Exercise1.py
+++ Exercise1.py
@@ -1,3 +1,4 @@
+#ATM
 secretcode =123456
 CurrentMoney=2000
 number=0
@@ -9,13 +10,13 @@ while number!=4:
               '3-Change secret code \n'
               '4-Exit \n')
         number=int(input())
-        if number==2:
+        if number==2: #Attract money
             print('How much money you want to attracth:'
                   '20,50 or another? \n')
             money=int(input())
             CurrentMoney-=money
-        elif number==1:
+        elif number==1: #Current money
             print('You have:' + str(CurrentMoney) + 'Shekels \n')
-        elif number==3:
+        elif number==3: # Enter new code
             code=int(input('Please enter your new secret code \n'))
             secretcode=code
